14608174b32b1b1d1103400a123ebbc3e56eed8c
ruyielder <ruyielder@gmail.com>
2014-12-05 16:12:57 +0100

    Updated ActiveAdmin Post permitted_params

--- app/admin/posts.rb
+++ app/admin/posts.rb
@@ -1,7 +1,7 @@
 ActiveAdmin.register Post do
   controller do
     def permitted_params
-      params.permit(:title , :slug, :content, :draft, :published_at)
+      params.permit!
     end
   end
 end
