6de5c5804f49ccbf9e2dab37e9bdd73b4e2a9c72
ruyielder <ruyielder@gmail.com>
2014-12-05 20:19:18 +0100

    Added title & published_at methods to PostPresenter

--- app/presenters/post_presenter.rb
+++ app/presenters/post_presenter.rb
@@ -6,10 +6,17 @@ class PostPresenter
     @post = post
   end
 
+  def title
+    @post.title
+  end
+
+  def published_at
+    @post.published_at
+  end
+
   def content
     renderer = Redcarpet::Render::HTML.new
     markdown = Redcarpet::Markdown.new(renderer)
     markdown.render(@post.content).html_safe
   end
-
 end
