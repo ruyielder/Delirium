7939eeb23c760ad090ed0c1c7ce65e8a251f1b7c
ruyielder <ruyielder@gmail.com>
2014-12-05 13:14:57 +0100

    Created Post factory

---
+++ spec/factories/posts.rb
@@ -0,0 +1,17 @@
+FactoryGirl.define do
+  factory :post do
+    title Faker::Name.title
+    content Faker::Lorem.paragraph
+    slug Faker::Internet.slug
+
+    factory :draft_post do
+      draft true
+      published_at nil
+    end
+
+    factory :published_post do
+      draft false
+      published_at Date.new(2014, 12, 5)
+    end
+  end
+end
