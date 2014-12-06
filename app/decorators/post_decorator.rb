9ab87fc5719a84d785614d41a002a3e1f34781d8
ruyielder <ruyielder@gmail.com>
2014-12-06 09:20:21 +0100

    Added method content to PostDecorator to render markdown

--- app/decorators/post_decorator.rb
+++ app/decorators/post_decorator.rb
@@ -1,7 +1,16 @@
+require 'redcarpet'
+
 class PostDecorator < Draper::Decorator
   delegate_all
 
   def link_to_post
     h.link_to title, h.post_path(slug)
   end
+
+  def content
+    renderer = Redcarpet::Render::HTML.new
+    markdown = Redcarpet::Markdown.new(renderer)
+    markdown.render(object.content).html_safe
+  end
+
 end
--- spec/decorators/post_decorator_spec.rb
+++ spec/decorators/post_decorator_spec.rb
@@ -1,10 +1,13 @@
 require 'spec_helper'
 
 describe PostDecorator do
-
   it 'has a link to self' do
     post = create(:published_post, title: 'hej').decorate
     expect(post.link_to_post).to eq('<a href="/posts/hej">hej</a>')
   end
 
+  it 'has content as markdown' do
+    post = create(:post, content: 'Hello, I **like** you!').decorate
+    expect(post.content).to eq("<p>Hello, I <strong>like</strong> you!</p>\n")
+  end
 end
