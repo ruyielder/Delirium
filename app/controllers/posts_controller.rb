f83405282fe7565f2c035b1fcd5855a8dbcc03e8
ruyielder <ruyielder@gmail.com>
2014-12-06 09:13:16 +0100

    Decorate posts in PostsController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,12 +2,12 @@ class PostsController < ApplicationController
   respond_to :html
 
   def index
-    @posts = Post.all
+    @posts = Post.all.decorate
     respond_with(@posts)
   end
 
   def show
-    @post = Post.friendly.find(params[:slug])
+    @post = Post.friendly.find(params[:slug]).decorate
     respond_with(@post)
   end
 end
