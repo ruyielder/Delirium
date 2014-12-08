f8d9e02934ac53c75f3b7450867150cd22e005ba
ruyielder <ruyielder@gmail.com>
2014-12-08 09:40:24 +0100

    Added validators to Tag

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,18 @@
 class Tag < ActiveRecord::Base
+  extend FriendlyId
+  friendly_id :name, use: :slugged
+
+  validates :name, :slug, presence: true
+  validates_uniqueness_of :slug
+
+  after_validation :move_slug_error_to_name
+
+  def move_slug_error_to_name
+    errors.add :name, *errors.delete(:slug) if errors[:slug].present?
+  end
+
+  # Disable resolve conflict because I need unique slugs without extra UUID..
+  def resolve_friendly_id_conflict(candidates)
+    candidates.first
+  end
 end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -4,4 +4,26 @@ describe Tag do
   it 'has a valid factory' do
     expect(create(:tag).valid?).to be true
   end
+
+  it 'is invalid without name' do
+    expect {create(:tag, name: '')}.to raise_error(ActiveRecord::RecordInvalid)
+  end
+
+  it 'has a valid slug' do
+    expect(create(:tag, name: 'Żółć jest żółta').slug).to eq 'zolc-jest-zolta'
+  end
+
+  it 'has a unique name' do
+    create(:tag, name: 'Hej')
+    expect {create(:tag, name: 'Hej')}.to raise_error(ActiveRecord::RecordInvalid)
+  end
+
+  it 'has moved errors from slug to name' do
+    create(:tag, name: 'Hej')
+    tag = build(:tag, name: 'Hej')
+    tag.save
+    expect(tag.valid?).to be false
+    expect(tag.errors.messages).to eq(name: ['has already been taken'])
+  end
+
 end
