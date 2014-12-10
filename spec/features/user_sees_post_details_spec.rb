9a92c3f80960a0f46b3c10dd2b59b73bec15972e
ruyielder <ruyielder@gmail.com>
2014-12-10 18:07:57 +0100

    Added to posts show view a humanized count of comments

--- spec/features/user_sees_post_details_spec.rb
+++ spec/features/user_sees_post_details_spec.rb
@@ -9,4 +9,13 @@ feature 'User sees post details' do
 
     expect(page).to have_text(post.title)
   end
+
+  scenario 'he sees humanized comment count' do
+    post = create(:published_post)
+    create(:comment_post, post: post)
+
+    visit post_path(slug: post.slug)
+
+    expect(page).to have_text('1 komentarz')
+  end
 end
