01af0084fa352d8e508945aefdc34d6d8ea033ba
ruyielder <ruyielder@gmail.com>
2014-12-10 17:03:56 +0100

    Fixed comment post factory

--- spec/factories/comment_posts.rb
+++ spec/factories/comment_posts.rb
@@ -9,12 +9,12 @@ FactoryGirl.define do
     end
 
     factory :comment_post_with_url do
-      email {Faker::Internet.url}
+      url {Faker::Internet.url}
     end
 
     factory :full_comment_post do
       email {Faker::Internet.email}
-      email {Faker::Internet.url}
+      url {Faker::Internet.url}
     end
   end
 end
