f33b9c8155e41bd14d4556b6b8e292665344e9d9
ruyielder <ruyielder@gmail.com>
2014-12-10 17:07:54 +0100

    Added validators to CommentPost

--- app/models/comment_post.rb
+++ app/models/comment_post.rb
@@ -1,3 +1,7 @@
 class CommentPost < ActiveRecord::Base
   belongs_to :post
+
+  validates :name, :content, presence: true
+  validates :email, format: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, allow_blank: true
+  validates :url, url: true, allow_blank: true
 end
--- spec/models/comment_post_spec.rb
+++ spec/models/comment_post_spec.rb
@@ -5,4 +5,34 @@ RSpec.describe CommentPost, :type => :model do
     post = create(:published_post)
     expect(build(:comment_post, post: post).valid?).to be true
   end
+
+  it 'is invalid without name' do
+    post = create(:published_post)
+    expect(build(:comment_post, post: post, name: '').valid?).to be false
+  end
+
+  it 'is invalid with bad formatted email' do
+    post = create(:published_post)
+    expect(build(:comment_post, post: post, email: 'booo').valid?).to be false
+  end
+
+  it 'has a valid factory with email' do
+    post = create(:published_post)
+    expect(build(:comment_post_with_email, post: post).valid?).to be true
+  end
+
+  it 'has a valid factory with email but with only spaces' do
+    post = create(:published_post)
+    expect(build(:comment_post, post: post, email: '     ').valid?).to be true
+  end
+
+  it 'is invalid with bad formatted url' do
+    post = create(:published_post)
+    expect(build(:comment_post, post: post, url: 'RUBY').valid?).to be false
+  end
+
+  it 'has a valid factory with url' do
+    post = create(:published_post)
+    expect(build(:comment_post_with_url, post: post).valid?).to be true
+  end
 end
