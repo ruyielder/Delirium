f8d4f1f7adbf1f34846d9b6b790e8dced42d9722
ruyielder <ruyielder@gmail.com>
2014-12-17 23:32:41 +0100

    Created HTMLwithGists

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,16 @@
+require 'redcarpet'
+
 module PostsHelper
+
+  class HTMLwithGists < Redcarpet::Render::HTML
+    def block_code(code, language)
+      snippet = Snippet.where(name: code.strip).first
+      create_script_markup(snippet.gist_url)
+    end
+
+    def create_script_markup(url)
+      "<script src='#{url}.js'></script>"
+    end
+  end
+
 end
