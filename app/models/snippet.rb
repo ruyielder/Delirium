1f5f982138808bbb0d96f99c395d5bf87eeed1c5
ruyielder <ruyielder@gmail.com>
2014-12-17 17:38:08 +0100

    Add validators to Snippet

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -1,3 +1,6 @@
 class Snippet < ActiveRecord::Base
   belongs_to :post
+
+  validates :name, :post, :source, presence: true
+  validates :gist_url, url: true, allow_blank: true
 end
--- spec/models/snippet_spec.rb
+++ spec/models/snippet_spec.rb
@@ -3,6 +3,25 @@ require 'rails_helper'
 RSpec.describe Snippet, :type => :model do
   it 'has a valid factory' do
     post = create(:post)
-    expect(create(:snippet, post: post).valid?).to be true
+    expect(build(:snippet, post: post)).to be_valid
+  end
+
+  it 'is invalid without name' do
+    post = create(:post)
+    expect(build(:snippet, name: '', post: post)).not_to be_valid
+  end
+
+  it 'is invalid without post' do
+    expect(build(:snippet, name: '')).not_to be_valid
+  end
+
+  it 'is invalid without source' do
+    post = create(:post)
+    expect(build(:snippet, source: '', post: post)).not_to be_valid
+  end
+
+  it 'is valid witout gist_url' do
+    post = create(:post)
+    expect(build(:snippet, gist_url: '', post: post)).to be_valid
   end
 end
