ff6305314ee93cb4b0b58ee0795106d918f8685c
ruyielder <ruyielder@gmail.com>
2014-12-17 18:05:27 +0100

    Added update_gist_url to call before snippet save

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -1,6 +1,24 @@
 class Snippet < ActiveRecord::Base
+  attr_writer :gist_client
+
   belongs_to :post
 
   validates :name, :post, :source, presence: true
   validates :gist_url, url: true, allow_blank: true
+
+  before_save :update_gist_url
+
+  def initialize(gist_client=nil, *args, **kwargs)
+    super(*args, **kwargs)
+    @gist_client = gist_client
+  end
+
+  def update_gist_url
+    response = gist_client.post_file(name, source)
+    self.gist_url = response['html_url']
+  end
+
+  def gist_client
+    @gist_client or SnippetsHelper::GistHttpClient.new
+  end
 end
--- spec/models/snippet_spec.rb
+++ spec/models/snippet_spec.rb
@@ -1,13 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Snippet, :type => :model do
+  let(:post) {create(:post)}
+  let(:gist_client) {SnippetsHelper::StubGistClient.new('html_url' => 'https://gist.github.com/123')}
+
   it 'has a valid factory' do
-    post = create(:post)
     expect(build(:snippet, post: post)).to be_valid
   end
 
   it 'is invalid without name' do
-    post = create(:post)
     expect(build(:snippet, name: '', post: post)).not_to be_valid
   end
 
@@ -16,12 +17,17 @@ RSpec.describe Snippet, :type => :model do
   end
 
   it 'is invalid without source' do
-    post = create(:post)
     expect(build(:snippet, source: '', post: post)).not_to be_valid
   end
 
   it 'is valid witout gist_url' do
-    post = create(:post)
     expect(build(:snippet, gist_url: '', post: post)).to be_valid
+  end
+
+  it 'update gist url before save' do
+    snippet = build(:snippet, post: post)
+    snippet.gist_client = gist_client
+    snippet.save
+    expect(snippet.gist_url).to eq('https://gist.github.com/123')
   end
 end
