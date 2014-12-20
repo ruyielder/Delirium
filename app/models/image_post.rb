a5c29aa9a3991ffa3eef50c2db147b7ea8c66469
ruyielder <ruyielder@gmail.com>
2014-12-20 23:34:32 +0100

    Added validators to ImagePost

--- app/models/image_post.rb
+++ app/models/image_post.rb
@@ -1,4 +1,7 @@
 class ImagePost < ActiveRecord::Base
   belongs_to :post
   mount_uploader :image, ImageUploader
+
+  validates :basename, format: { with: /\A\p{Word}+\z/ }
+  validates :image, presence: true
 end
--- spec/models/image_post_spec.rb
+++ spec/models/image_post_spec.rb
@@ -1,8 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe ImagePost, :type => :model do
+  let(:post) {create(:published_post)}
+
   it 'has a valid factory' do
-    post = create(:published_post)
     expect(build(:image_post, post: post)).to be_valid
+  end
+
+  it 'is invalid when basename contains white characters' do
+    expect(build(:image_post, post: post, basename: 'first  second')).not_to be_valid
+  end
+
+  it 'is invalid when basename contains special characters' do
+    expect(build(:image_post, post: post, basename: 'file.txt')).not_to be_valid
+  end
+
+  it 'is valid when basename contains polish characters' do
+    expect(build(:image_post, post: post, basename: 'żółęść')).to be_valid
+  end
+
+  it 'is valid when basename is emtpy' do
+    expect(build(:image_post, post: post, basename: '')).not_to be_valid
+  end
+
+  it 'is invalid when image is empty' do
+    expect(build(:image_post, post: post, image: '')).not_to be_valid
   end
 end
