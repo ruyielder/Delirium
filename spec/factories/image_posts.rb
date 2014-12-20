992b30c4592d7633976197b9844b6089b6a38629
ruyielder <ruyielder@gmail.com>
2014-12-20 23:31:12 +0100

    Fixed basename of image post factory

--- spec/factories/image_posts.rb
+++ spec/factories/image_posts.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :image_post do
-    basename {Faker::Name.name}
+    basename {Faker::Lorem.word}
     image Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec', 'support', 'factories', 'bird.png'))
   end
 end
