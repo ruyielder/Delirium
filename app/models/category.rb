96fb1a2799f17819c93ffb8a118b886cec3d4e06
ruyielder <ruyielder@gmail.com>
2014-12-22 21:08:17 +0100

    Added friendly_id to Category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,16 @@
 class Category < ActiveRecord::Base
+  extend FriendlyId
+  friendly_id :name, use: :slugged
+
+  after_validation :move_slug_error_to_name
+  validates_uniqueness_of :slug
+
+  def move_slug_error_to_name
+    errors.add :name, *errors.delete(:slug) if errors[:slug].present?
+  end
+
+  # Disable resolve conflict because I need unique slugs without extra UUID..
+  def resolve_friendly_id_conflict(candidates)
+    candidates.first
+  end
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -6,4 +6,16 @@ RSpec.describe Category, :type => :model do
     expect(build(:category)).to be_valid
   end
 
+  it 'has a valid slug' do
+    expect(create(:category, name: 'Żółć jest żółta').slug).to eq 'zolc-jest-zolta'
+  end
+
+  it 'has moved errors from slug to name' do
+    create(:category, name: 'Hej')
+    category = build(:category, name: 'Hej')
+    category.save
+    expect(category).not_to be_valid
+    expect(category.errors.messages).to eq(name: ['has already been taken'])
+  end
+
 end
