cd71a09c92aefc95313637054441f78eae831378
ruyielder <ruyielder@gmail.com>
2014-12-26 19:59:46 +0100

    Added length validators for CommentPost

--- app/models/comment_post.rb
+++ app/models/comment_post.rb
@@ -6,5 +6,10 @@ class CommentPost < ActiveRecord::Base
   validates :email, format: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, allow_blank: true
   validates :url, url: true, allow_blank: true
 
+  validates_length_of :name, minimum: 2, maximum: 30
+  validates_length_of :email, minimum: 8, maximum: 150, allow_blank: true
+  validates_length_of :url, minimum: 5, maximum: 250, allow_blank: true
+  validates_length_of :content, minimum: 3, maximum: 1000
+
   scope :published, -> {where(blocked: false)}
 end
